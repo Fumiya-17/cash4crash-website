de64af479abbf48297251079081d62ebee6a33c2
Fumiya-17 <ratunil.ralo17@gmail.com>
2026-05-27 18:02:52 +0800

    db

--- backend/setup_reporting_db.py
+++ backend/setup_reporting_db.py
@@ -1,7 +1,7 @@
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-conn = psycopg2.connect("dbname='postgres' user='postgres' password='admin' host='localhost'")
+conn = psycopg2.connect("dbname='postgres' user='postgres' password='admin' host='db'")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
 try:
